0258c4ec94fe3323d68ff918587f5115de517637
CSE <revengerreigns076@gmail.com>
2020-11-14 21:26:33 +0600

    Changing App Icon

---
+++ lib/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Some flutter packages for loading animation
+// 1.https://pub.dev/packages/circular_custom_loader
+// 2.https://pub.dev/packages/progress_state_button
+// 3.https://pub.dev/packages/flutter_spinkit
