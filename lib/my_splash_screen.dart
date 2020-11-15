c37c1b2fa4d4a20f5af88f6405f6ea23876c10d6
CSE <revengerreigns076@gmail.com>
2020-11-15 08:28:42 +0600

    Checking error solved or not

--- lib/my_splash_screen.dart
+++ lib/my_splash_screen.dart
@@ -38,3 +38,4 @@ class MySplashScreen extends StatelessWidget {
 // 1.https://pub.dev/packages/circular_custom_loader
 // 2.https://pub.dev/packages/progress_state_button
 // 3.https://pub.dev/packages/flutter_spinkit
+//https://pub.dev/packages/percent_indicator
